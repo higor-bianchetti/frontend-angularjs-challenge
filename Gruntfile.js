a4a0c897be8b42482f255a6059d2603b4898214f
Higor <higorbianchetti@gmail.com>
2023-06-07 23:43:24 -0300

    Setting up livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,53 +3,102 @@ module.exports = function(grunt) {
 	  pkg: grunt.file.readJSON('package.json'),
 
  	  concat: {
- 		 options: {
-    			separator: ';'
-  		 },
-  	  	 dist: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				files: {
+					'build/strands/strands.json': ['app/strands/strands.json'],
+					'build/popup.html': ['app/popup.html'],
+				}
+			}
+ 	 	},
+
+		uglify: {
+			my_target: {
 			 files: {
-    			'build/strands/strands.json': ['app/strands/strands.json'],
-    			'build/popup.html': ['app/popup.html'],
-			}
-  		 }
- 	 },
-
-	 uglify: {
-	 	my_target: {
-			files: {
-				'build/js/script.min.js': ['app/js/*.js']
-			}
-		}
-	 },
-
-	 cssmin: {
-	 	options: {
-			shorthandCompacting: false,
-			roundingPrecision: -1
+				 'build/js/script.min.js': ['app/js/app.js', 'app/js/services/**/*.js', 'app/js/controllers/**/*.js', 'app/js/*.js']
+			 }
+		 }
 		},
-		target: {
-			files: {
-				'build/css/style.min.css': ['app/css/*.css']
-			}
-		}
-	 },
 
-	 processhtml: {
-	 	options: {
+	 	cssmin: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: -1
+			},
+			target: {
+				files: {
+					'build/css/style.min.css': ['app/css/*.css']
+				}
+			}
 		},
-	 	dist: {
-			files: {
-				'build/index.html': ['app/index.html']
+
+	 	processhtml: {
+			options: {
+			},
+			dist: {
+				files: {
+					'build/index.html': ['app/index.html']
+				}
+			}
+		},
+
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true,
+						flatten: true,
+						src: ['app/partials/*'],
+						dest: 'build/partials', filter: 'isFile'
+					},
+				]
+			}
+		},
+
+		connect: {
+			dev: {
+				options: {
+					port: 9000,
+					base: {
+						path: 'app',
+						options: {
+							index: 'index.html',
+							maxAge: 300000
+						}
+					}
+				}
+			},
+		},
+
+		open: {
+			dev : {
+				path: 'http://localhost:9000',
+				app: 'Chrome'
+			}
+		},
+
+		watch: {
+			client: {
+				files: ['app/**/*'],
+				tasks:['concat', 'cssmin', 'copy', 'processhtml'],
+				options: {
+					livereload: true
+				}
 			}
 		}
-	}
-  })
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-processhtml');
-grunt.registerTask('build', ['concat','uglify', 'cssmin', 'processhtml']);
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-processhtml');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-open');
+
+	grunt.registerTask('build', ['concat', 'uglify', 'cssmin', 'processhtml']);
+	grunt.registerTask('dev', ['connect:dev', 'open:dev', 'watch:client']);
 };
-
-
-
